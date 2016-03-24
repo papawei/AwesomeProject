569d3b4ecdd2c64d4c7e1b8190c94049d6f43da1
papawei <137294593@qq.com>
2016-03-24 18:31:10 +0800

    新建项目

---
+++ src/reducers/reducer.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2016/3/24.
+ */
+'use strict';
+
+import {combineReducers} from 'redux';
+import index from './index';
+
+
+const reducer = combineReducers({
+  index
+})
+
+export default reducer;
---
+++ src/store/store.js
@@ -0,0 +1,23 @@
+
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+
+import reducer from './src/reducers/reducer';
+
+const middleWares = [
+  thunkMiddleware,
+];
+
+if (__DEV__) {
+  middleWares.unshift(createLogger());
+}
+
+const createStoreWithMiddleware = applyMiddleware(
+  ...middleWares
+)(createStore);
+
+export default function configureStore(initialState) {
+  const store = createStoreWithMiddleware(reducer, initialState);
+  return store;
+}
